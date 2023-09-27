e84fac7afa2c99d3bbdca4f815f4277c992b9a03
Samuel-Chapuis <samuel.chapuis@hotmail.com>
2023-09-28 00:14:39 +0200

    On supprimes les tsbs

--- TestRecoImage/screen.py
+++ TestRecoImage/screen.py
@@ -15,15 +15,15 @@ import faceDetection as faceDetection
 
 #Mainc
 while True:
-	frame = cv.cvtColor(np.array(pyautogui.screenshot()), cv.COLOR_RGB2BGR)		# Take screenshot
-	frame = rescale.rescaleFrame(frame, scale=0.5)								# Rescale frame
+	frame = cv.cvtColor(np.array(pyautogui.screenshot()), cv.COLOR_RGB2BGR)     # Take screenshot
+	frame = rescale.rescaleFrame(frame, scale=0.5)                              # Rescale frame
 	
-	detect_faces = faceDetection.detect_faces(frame)							# Detect faces in the frame
-	for (x, y, w, h) in detect_faces:											# Draw rectangles around faces
+	detect_faces = faceDetection.detect_faces(frame)                            # Detect faces in the frame
+	for (x, y, w, h) in detect_faces:                                           # Draw rectangles around faces
 		cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-	cv.imshow('frame', frame)													# Display frame
-	if cv.waitKey(1) & 0xFF == 27:												# Wait for 'esc' to quit
+	cv.imshow('frame', frame)                                                   # Display frame
+	if cv.waitKey(1) & 0xFF == 27:                                              # Wait for 'esc' to quit
 		print("Quiting...")
 		break
 
--- TestRecoImage/webcam.py
+++ TestRecoImage/webcam.py
@@ -14,17 +14,17 @@ import faceDetection as faceDetection
 vid = cv.VideoCapture(0)
 
 while True:
-	success, frame = vid.read()											# Read frame by frame	
-	frame = rescale.rescaleFrame(frame, scale=1.2)						# Rescale frame
+	success, frame = vid.read()                                         # Read frame by frame	
+	frame = rescale.rescaleFrame(frame, scale=1.2)                      # Rescale frame
 
-	detect_faces = faceDetection.detect_faces(frame)					# Detect faces in the frame
-	for (x, y, w, h) in detect_faces:									# Draw rectangles around faces
+	detect_faces = faceDetection.detect_faces(frame)                    # Detect faces in the frame
+	for (x, y, w, h) in detect_faces:                                   # Draw rectangles around faces
 		cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 
 
-	cv.imshow('frame', frame)											# Display frame
-	if cv.waitKey(1) & 0xFF == 27:										# Wait for 'esc' to quit
+	cv.imshow('frame', frame)                                           # Display frame
+	if cv.waitKey(1) & 0xFF == 27:                                      # Wait for 'esc' to quit
 		print("Quiting...")
 		break
 
