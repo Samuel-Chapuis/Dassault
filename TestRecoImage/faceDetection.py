01496a821df71afccd69f1614b4b3830a3551ae7
Samuel-Chapuis <samuel.chapuis@hotmail.com>
2023-09-28 00:11:06 +0200

    On change les sortie des faceDetection

--- TestRecoImage/faceDetection.py
+++ TestRecoImage/faceDetection.py
@@ -9,13 +9,7 @@ import	cv2	as	cv
 face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 def detect_faces(frame):
-    # Convert the frame to grayscale for face detection
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Detect faces in the grayscale frame
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-    # Draw rectangles around the detected faces
-    for (x, y, w, h) in faces:
-        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    return frame
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)                                                        # Convert the frame to grayscale for face detection
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))      # Detect faces in the grayscale frame
+    face_coordinates = [(x, y, w, h) for (x, y, w, h) in faces]                                         # Get the coordinates of the faces
+    return face_coordinates
--- TestRecoImage/screen.py
+++ TestRecoImage/screen.py
@@ -18,7 +18,9 @@ while True:
 	frame = cv.cvtColor(np.array(pyautogui.screenshot()), cv.COLOR_RGB2BGR)		# Take screenshot
 	frame = rescale.rescaleFrame(frame, scale=0.5)								# Rescale frame
 	
-	frame = faceDetection.detect_faces(frame)													# Detect faces in the frame
+	detect_faces = faceDetection.detect_faces(frame)							# Detect faces in the frame
+	for (x, y, w, h) in detect_faces:											# Draw rectangles around faces
+		cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 	cv.imshow('frame', frame)													# Display frame
 	if cv.waitKey(1) & 0xFF == 27:												# Wait for 'esc' to quit
